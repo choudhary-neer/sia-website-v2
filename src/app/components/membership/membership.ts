345caaf0205ba064a1f3e8af7981743e2e933350
Neeraj Choudhary_c <neeraj.choudhary_c@sandvik.com>
2026-03-15 22:40:37 +0100

    updated membership page

--- src/app/components/membership/membership.ts
+++ src/app/components/membership/membership.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { LanguageService } from '../../services/language.service';
 import { ContentModel } from '../../models/content';
+import { ContactValuesService } from '../../services/contact-values.service';
+import { Contact } from '../../models/contact';
 
 @Component({
   selector: 'app-membership',
@@ -13,12 +15,19 @@ import { ContentModel } from '../../models/content';
 })
 export class MembershipComponent implements OnInit {
   content!: ContentModel;
+  contact!: Contact;
 
-  constructor(private languageService: LanguageService) {}
+  constructor(
+    private languageService: LanguageService,
+    private contactValuesService: ContactValuesService
+  ) {}
 
   ngOnInit() {
     this.languageService.currentLang$.subscribe(() => {
       this.content = this.languageService.getContent();
+    });
+    this.contactValuesService.getContactValues().subscribe(data => {
+      this.contact = data;
     });
   }
 }
