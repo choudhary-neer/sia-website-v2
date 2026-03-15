575729352d526890a4fd4e34ffd0c3d449527e7f
Neeraj Choudhary_c <neeraj.choudhary_c@sandvik.com>
2026-03-15 22:26:20 +0100

    update announcements from json

--- src/app/components/announcements/announcements.ts
+++ src/app/components/announcements/announcements.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { LanguageService } from '../../services/language.service';
 import { RouterModule } from '@angular/router';
 import { ContentModel } from '../../models/content';
+import { AnnouncementsService } from '../../services/announcements.service';
+import { Announcement } from '../../models/announcement';
 
 @Component({
   selector: 'app-announcements',
@@ -13,12 +15,20 @@ import { ContentModel } from '../../models/content';
 })
 export class AnnouncementsComponent implements OnInit {
   content!: ContentModel;
+  announcements: Announcement[] = [];
 
-  constructor(private languageService: LanguageService) {}
+  constructor(
+    private languageService: LanguageService,
+    private announcementsService: AnnouncementsService
+  ) {}
 
   ngOnInit() {
     this.languageService.currentLang$.subscribe(() => {
       this.content = this.languageService.getContent();
+    });
+
+    this.announcementsService.getAnnouncements().subscribe(data => {
+      this.announcements = data;
     });
   }
 }
