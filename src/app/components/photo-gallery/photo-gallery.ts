66cf1de94a82d7ce2f4bec6b3191bee85d47a760
Neeraj Choudhary_c <neeraj.choudhary_c@sandvik.com>
2026-03-21 07:59:38 +0100

    updated photo gallary

--- src/app/components/photo-gallery/photo-gallery.ts
+++ src/app/components/photo-gallery/photo-gallery.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { LanguageService } from '../../services/language.service';
@@ -13,6 +13,42 @@ import { ContentModel } from '../../models/content';
 })
 export class PhotoGalleryComponent implements OnInit {
   content!: ContentModel;
+  currentIndex = 0;
+  readonly images = [
+    {
+      src: 'assets/photo-gallery/sandviken-1.jpg',
+      alt: 'Sandvieken City 1'
+    },
+    {
+      src: 'assets/photo-gallery/sandviken-2.jpg',
+      alt: 'Sandvieken City 2'
+    },
+    {
+      src: 'assets/photo-gallery/sandviken-3.jpg',
+      alt: 'Sandvieken City 3'
+    },
+    {
+      src: 'assets/photo-gallery/sandviken-4.jpg',
+      alt: 'Sandvieken City 4'
+    },
+    {
+      src: 'assets/photo-gallery/sandviken-5.jpg',
+      alt: 'Sandvieken City 5'
+    },
+    {
+      src: 'assets/photo-gallery/sandviken-6.jpg',
+      alt: 'Sandvieken City 6'
+    },
+    {
+      src: 'assets/photo-gallery/sandviken-7.jpg',
+      alt: 'Sandvieken City 7'  
+    },
+    {
+      src: 'assets/photo-gallery/sandviken-8.jpg',
+      alt: 'Sandvieken City 8'
+    }
+  ];
+  private autoplayHandle: ReturnType<typeof setInterval> | null = null;
 
   constructor(private languageService: LanguageService) {}
 
@@ -20,5 +56,45 @@ export class PhotoGalleryComponent implements OnInit {
     this.languageService.currentLang$.subscribe(() => {
       this.content = this.languageService.getContent();
     });
+
+    this.startAutoplay();
+  }
+
+  ngOnDestroy(): void {
+    this.stopAutoplay();
+  }
+
+  nextSlide(): void {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+
+  prevSlide(): void {
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
+
+  goToSlide(index: number): void {
+    this.currentIndex = index;
+  }
+
+  pauseAutoplay(): void {
+    this.stopAutoplay();
+  }
+
+  resumeAutoplay(): void {
+    this.startAutoplay();
+  }
+
+  private startAutoplay(): void {
+    this.stopAutoplay();
+    this.autoplayHandle = setInterval(() => {
+      this.nextSlide();
+    }, 4000);
+  }
+
+  private stopAutoplay(): void {
+    if (this.autoplayHandle) {
+      clearInterval(this.autoplayHandle);
+      this.autoplayHandle = null;
+    }
   }
 }
